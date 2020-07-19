dc2c3013916fb7e90716ce8a6f3a5082cfe9097b
UthpalaK97 <kasunuthpalak@gmail.com>
2020-07-19 09:10:07 +0530

    Added TheCaesarCipher.scala

---
+++ Assignment_The Caesar Cipher/TheCaesarCipher.scala
@@ -0,0 +1,24 @@
+object CeasarCipher extends App{
+    val alphabet = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+   
+    val encryption = (c : Char , key : Int , a : String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+    val decryption = (c : Char , key : Int , a : String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+    
+    val cipher = (algo : (Char, Int, String)=> Char, s:String, key:Int, a:String)=>s.map(algo(_,key,a))
+    
+    
+    val text = scala.io.StdIn.readLine("Massage : ")
+    val keyVal = (scala.io.StdIn.readLine("Key : ").toInt+alphabet.size)%alphabet.size
+    val mode= scala.io.StdIn.readLine("Encryption(E) / Decryption(D) : ").toUpperCase()
+   
+   
+    if(mode=="E") {
+       println(cipher(encryption,text,keyVal,alphabet))} 
+    else if (mode=="D") {
+       println(cipher(decryption,text,keyVal,alphabet))}
+    else {
+        println("***Invalid Input***")
+    }
+    
+    
+}
